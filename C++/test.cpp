635bb8d0b914081c8a70d7dbfd3ce7b0f4d10090
RikhardHonkanen <rikhard@raket.co>
2025-01-08 01:20:47 +0100

    compiler settings etc

---
+++ C++/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// This program prints out “Hello World!”
+int main()
+{
+    string message = "Hello World!\n";
+    cout << message;
+    int a = 2;
+    int b = 3;
+    printf("%d", a);
+    return 0;
+};
