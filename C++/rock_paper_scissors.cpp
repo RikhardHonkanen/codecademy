52a0d6e4d5451afabaa4370b25eaba41c72bfae1
RikhardHonkanen <rikhard@raket.co>
2025-04-16 09:13:46 +0200

    rps excersise init

---
+++ C++/rock_paper_scissors.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+/*
+Game:
+Rock, Paper, Scissors, Lizard, Spock
+
+Rules:
+Scissors cuts Paper.
+Paper covers Rock.
+Rock crushes Lizard.
+Lizard poisons Spock.
+Spock smashes Scissors.
+Scissors decapitate Lizard.
+Lizard eats Paper.
+Paper disproves Spock.
+Spock vaporizes Rock.
+(and as it always has) Rock crushes Scissors.
+*/
+
+int main()
+{
+    // Live long and prosper
+}
